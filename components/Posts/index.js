1507d8d2e5a864c966897964cdbb45c678373850
exactchange <benny@exactchange.network>
2022-11-21 06:46:48 -0800

    parse links

--- components/Posts/index.js
+++ components/Posts/index.js
@@ -1,23 +1,33 @@
 import styles from '../../styles/Home.module.css';
 
-export const Posts = ({ posts }) => (
-  <div>
-    <p className={styles.description}>
-      <input
-        placeholder="Search"
-        role="search"
-        className={styles.search}
-        disabled
-      />
-    </p>
-    {
-      posts.map(({ author, text, date }) => (
-        <div key={text} className={styles.card}>
-          <a href="#">{`@${author}`}</a>
-          <p>{text}</p>
-          <a href="#">{date}</a>
-        </div>
-      ))
-    }
-  </div>
+const parseLinks = text => (
+  text.replace(/(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig,
+    url => (
+      `<a href="${url}">${url}</a>`
+    )
+  )
 );
+
+export const Posts = ({ posts }) => {
+  return (
+    <div>
+      <p className={styles.description}>
+        <input
+          placeholder="Search"
+          role="search"
+          className={styles.search}
+          disabled
+        />
+      </p>
+      {
+        posts.map(({ author, text, date }) => (
+          <div key={text} className={styles.card}>
+            <a href="#">{`@${author}`}</a>
+            <p dangerouslySetInnerHTML={{ __html: parseLinks(text) }} />
+            <a href="#">{date}</a>
+          </div>
+        ))
+      }
+    </div>
+  );
+}
